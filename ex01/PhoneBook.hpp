70b53a8ba72523df97aa8ec90cd1ec0e445aff14
ccosta-c <ccosta-c@student.42porto.com>
2024-02-01 17:12:09 +0000

    Initial commit

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 14:29:36 by ccosta-c          #+#    #+#             */
+/*   Updated: 2024/01/29 14:29:41 by ccosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef C00_PHONEBOOK_H
+#define C00_PHONEBOOK_H
+
+
+class	Contact {
+	public:
+		char	FirstName;
+		char	LastName;
+		char	Nickname;
+		int		PhoneNumber;
+		char	DarkestSecret;
+};
+
+class	PhoneBook {
+	public:
+
+};
+
+#endif
