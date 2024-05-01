8f77ceea6424335a3b3d5ee52d421d02b165240f
ccosta-c <ccosta-c@student.42porto.com>
2024-05-01 16:28:31 +0100

    gonna have to change the assignment operator in all the parent classes cause im only changing the clpatrap values

--- C03/ex03/DiamondTrap.cpp
+++ C03/ex03/DiamondTrap.cpp
@@ -1,5 +1,18 @@
-//
-// Created by ccosta-c on 5/1/24.
-//
 
 #include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap(std::string name)
+{
+	this->_name = name;
+	ClapTrap::_name = name.append("_clap_name");
+	_hitPoints = FragTrap::_hitPoints;
+	_energyPoints =ScavTrap::_energyPoints;
+	_attackDamage = FragTrap::_attackDamage;
+	std::cout << "Fresh off the assembly line, the monstrous" << GRN << " DiamondTrap " << _name << NRM " spurs to life!" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(DiamondTrap &copy)
+{
+	*this = copy;
+}
+
--- C03/ex03/DiamondTrap.hpp
+++ C03/ex03/DiamondTrap.hpp
@@ -2,17 +2,18 @@
 #ifndef DIAMONDTRAP_HPP
 #define DIAMONDTRAP_HPP
 
-class DiamondTrap {
+#include "FragTrap.hpp"
+
+class DiamondTrap : public FragTrap
+{
   private:
-
+	std::string _name;
   public:
-	DiamondTrap();
-
+	DiamondTrap(std::string name);
 	DiamondTrap(DiamondTrap &copy);
-
 	DiamondTrap &operator=(const DiamondTrap &values);
-
 	~DiamondTrap();
+	void whoAmI();
 };
 
 #endif
